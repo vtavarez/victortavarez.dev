df6aac0afb8e8167e5f3988da1867b60804dca27
Victor Tavarez <victor.j.tavarez@gmail.com>
2019-09-03 16:35:26 -0700

    Initial commit

--- data/config.js
+++ data/config.js
@@ -5,7 +5,7 @@ module.exports = {
 	url: 'https://victortavarez.dev',
 	legalName: 'Victor Tavarez',
 	defaultDescription:
-		"Hi there! I'm Victor and I'm a Front-End Engineer! Besides cooking up super awesome 🔥 UI's for clients! I also like to work on open source projects, blog about ✨ new frameworks and technologies, and take long walks on the beach...jk..no but really. 😭",
+		"Hi there! I'm Victor and I'm a Front-End Engineer! Besides cooking up super awesome 🔥 UI's for clients! I also like to work on open source projects, blog about ✨ new frameworks and technologies, and take long walks on the beach...jk 😂 ",
 	socialLinks: {
 		twitter: 'http://www.twitter.com/imvictortavarez',
 		github: 'https://github.com/vtavarez',
