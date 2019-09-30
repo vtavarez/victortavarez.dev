66e2eeed2ec497c774c05bc77134a550114ad61a
Victor Tavarez <victor.j.tavarez@gmail.com>
2019-09-30 00:01:53 -0700

    done

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,14 +18,14 @@ module.exports = {
 			resolve: 'gatsby-source-filesystem',
 			options: {
 				name: 'blog',
-				path: `${__dirname}/src/blog`,
+				path: `${__dirname}/src/blog/`,
 			},
 		},
 		{
 			resolve: 'gatsby-source-filesystem',
 			options: {
 				name: 'tags',
-				path: `${__dirname}/src/blog/tags`,
+				path: `${__dirname}/src/blog/tags/`,
 			},
 		},
 		'gatsby-transformer-sharp',
