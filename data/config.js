047720646c3f77a0aded27cc38a884db7dc644d8
Victor Tavarez <victor.j.tavarez@gmail.com>
2019-11-02 18:32:33 -0700

    Refactor config.js

--- data/config.js
+++ data/config.js
@@ -1,11 +1,11 @@
 module.exports = {
-	defaultTitle: 'Victor Tavarez | Front-End Engineer',
+	defaultTitle: 'Victor Tavarez | Front-End Developer',
 	logo: 'https://victortavarez.dev/favicon/favicon-512.png',
 	author: 'Victor Tavarez',
 	url: 'https://victortavarez.dev',
 	legalName: 'Victor Tavarez',
 	defaultDescription:
-		"Hi there! I'm Victor and I'm a Front-End Engineer! Besides cooking up awesome UI's for clients! I also like to work on open source projects, and blog about current and upcoming web technologies.",
+		'Front-end developer with a focus on delivering high quality, performant web applications for clients.',
 	socialLinks: {
 		twitter: 'http://www.twitter.com/imvictortavarez',
 		github: 'https://github.com/vtavarez',
@@ -14,18 +14,18 @@ module.exports = {
 	themeColor: '#2ecc71',
 	backgroundColor: '#ffffff',
 	social: {
-		facebook: 'appId',
+		facebook: '',
 		twitter: '@imvictortavarez',
 	},
 	address: {
-		city: 'City',
-		region: 'Region',
-		country: 'Country',
-		zipCode: 'ZipCode',
+		city: '',
+		region: '',
+		country: '',
+		zipCode: '',
 	},
 	contact: {
-		email: 'email',
-		phone: 'phone number',
+		email: '',
+		phone: '',
 	},
 	foundingDate: '2019',
 	recaptcha_key: '6Lct_roUAAAAALQqBHA5R2nDxYLPnHsedO7Nxnrf',
