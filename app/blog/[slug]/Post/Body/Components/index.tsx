4f8cb72a72b970c2c04366b2998f35ca36a77ef7
Victor Tavarez <victor@Victors-MacBook-Pro.local>
2024-02-06 23:18:59 -0800

    Blog post page body blockquote component added.

--- app/blog/[slug]/Post/Body/Components/index.tsx
+++ app/blog/[slug]/Post/Body/Components/index.tsx
@@ -21,6 +21,11 @@ export const Components: PortableTextComponents = {
     h3: ({ children }) => (
       <h3 className="my-4 text-2xl font-bold">{children}</h3>
     ),
+    blockquote: ({ children }) => (
+      <blockquote className="my-4 border-l-4 pl-4 italic">
+        {children}
+      </blockquote>
+    ),
   },
   types: {
     code: ({
