307a565d37b406f7cf8382869955b8de0586f91a
Victor Tavarez <victor.j.tavarez@gmail.com>
2024-03-13 15:23:05 -0700

    Webhook posts routes updated for better error handling.

--- app/webhook/posts/route.ts
+++ app/webhook/posts/route.ts
@@ -1,11 +1,14 @@
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { revalidateTag } from 'next/cache';
 export async function POST(request: NextRequest) {
 	try {
 		revalidateTag('posts');
-		return new Response('OK');
+		return NextResponse.json({ message: 'Posts revalidated' }, { status: 200 });
 	} catch (error) {
 		console.error(error);
-		return new Response('Error');
+		return NextResponse.json(
+			{ message: 'Error revalidating posts' },
+			{ status: 500 },
+		);
 	}
 }
