11827d57f5226f68b40176f84cf32ba5aa6078a1
vtavarez <victor.j.tavarez@gmail.com>
2024-03-14 19:49:08 -0700

    Testing contact form.

--- components/theme/ContactForm/index.tsx
+++ components/theme/ContactForm/index.tsx
@@ -60,24 +60,24 @@ export function ContactForm() {
 
 	async function onSubmit(data: Inputs) {
 		try {
-			const token = executeRecaptcha ? await executeRecaptcha('submit') : '';
-			const verified = await verify(token);
+			// const token = executeRecaptcha ? await executeRecaptcha('submit') : '';
+			// const verified = await verify(token);
 
-			if (verified.success) {
-				const response = await send(data);
+			// if (verified.success) {
+			const response = await send(data);
 
-				if ('error' in response) {
-					setSubmitStatus({ ...submitStatus, isError: true });
-					return;
-				}
-
-				reset();
-				setTimeout(
-					() => setSubmitStatus({ ...submitStatus, isSuccess: false }),
-					5000,
-				);
-				setSubmitStatus({ ...submitStatus, isSuccess: true });
+			if ('error' in response) {
+				setSubmitStatus({ ...submitStatus, isError: true });
+				return;
 			}
+
+			reset();
+			setTimeout(
+				() => setSubmitStatus({ ...submitStatus, isSuccess: false }),
+				5000,
+			);
+			setSubmitStatus({ ...submitStatus, isSuccess: true });
+			// }
 		} catch (err) {
 			console.error(err);
 			setSubmitStatus({ ...submitStatus, isError: true });
