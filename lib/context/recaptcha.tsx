dfb2daa91ce88aa6819c645cd461a656c451a654
vtavarez <victor.j.tavarez@gmail.com>
2024-03-14 19:30:20 -0700

    Testing contact form.

--- lib/context/recaptcha.tsx
+++ lib/context/recaptcha.tsx
@@ -1,18 +1,18 @@
-"use client";
-import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+'use client';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 export function ReCaptchaProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <GoogleReCaptchaProvider
-      reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ""}
-      scriptProps={{
-        async: false,
-        defer: true,
-        appendTo: "head",
-        nonce: undefined,
-      }}
-    >
-      {children}
-    </GoogleReCaptchaProvider>
-  );
+	return (
+		<GoogleReCaptchaProvider
+			reCaptchaKey="6LdpWE8pAAAAAFOiaw3g6Pia4mT1pHtfWpHlePIi"
+			scriptProps={{
+				async: false,
+				defer: true,
+				appendTo: 'head',
+				nonce: undefined,
+			}}
+		>
+			{children}
+		</GoogleReCaptchaProvider>
+	);
 }
